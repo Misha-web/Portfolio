55821e23f7c0c39a2def7c11c97dc89cc113b43e
Misha-web <60877381+Misha-web@users.noreply.github.com>
2020-05-05 18:32:47 +0300

    blocks works and skilks is made

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
+	//Burger menu
 	$(".header__burger").on("click", function(event) {
 		event.preventDefault();
 		$(".nav,.header__burger").toggleClass('active');
-	})
+	});
+
+	//Smoove scroll
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+		let blockId = $(this).data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
+	});
 });
 
 
